b69a6fc3fd90d060f95e993cf6f455f0fa5ffa5d
Yolo-He <2684921675@qq.com>
2022-03-22 00:16:30 +0800

    深度优先搜索+回溯/分治

--- 剑指 Offer 55 - I. 二叉树的深度.js
+++ 剑指 Offer 55 - I. 二叉树的深度.js
@@ -9,24 +9,53 @@
  * @param {TreeNode} root
  * @return {number}
  */
+//法1：深搜+回溯  可以看出每条路径，但时间空间占用大，差的算法
 var maxDepth = function(root) {
     if (root == null) return 0;
-
     let stark = [];
     let deep = 1;
-
     let dfs = function(root) {
         stark.push(root.val);
-        if (root.left == null && root.right == null) deep = Math.max(deep, stark.length);
+        if (root.left == null && root.right == null) {
+            deep = Math.max(deep, stark.length);
+        }
         if (root.left != null) {
             dfs(root.left);
-        }
-
-        if (root.right != null) {
             stark.pop();
+        }
+        if (root.right != null) {
             dfs(root.right);
+            stark.pop();
         }
     }
     dfs(root);
     return deep;
+};
+
+// 法2：深搜 + 分治，优秀
+var maxDepth = function(root) {
+    if (root === null) return 0;
+
+    let depthLeft = maxDepth(root.left);
+    let depthRight = maxDepth(root.right);
+
+    return (depthLeft > depthRight ? depthLeft : depthRight) + 1;
+};
+
+// 法3：先序深度搜索 + 回溯， 优秀
+var maxDepth = function(root) {
+    if (root == null) return 0;
+
+    let deep = 0,
+        res = 0;
+    let dfs = function(root) {
+        deep++;
+        res = deep > res ? deep : res;
+        if (root.left != null) dfs(root.left);
+        if (root.right != null) dfs(root.right);
+        deep--;
+    }
+
+    dfs(root);
+    return res;
 };
