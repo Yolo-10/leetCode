5e24941071f206805aea22f0557019f625ed4eb4
Yolo-He <2684921675@qq.com>
2022-04-07 00:06:13 +0800

    链表实现两束相加

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -16,23 +16,21 @@ var addTwoNumbers = function(l1, l2) {
         summy = 0;
     l3 = new ListNode();
     root = l3;
-    while (l1 != null || l2 != null) {
-        if (l1 == null) {
-            n1 = 0;
-        } else {
+    while (l1 != null || l2 != null || summy != 0) {
+        if (l1 == null) n1 = 0;
+        else {
             n1 = l1.val;
             l1 = l1.next;
         }
 
-        if (l2 == null) {
-            n2 = 0;
-        } else {
+        if (l2 == null) n2 = 0;
+        else {
             n2 = l2.val;
             l2 = l2.next;
         }
 
         temp = new ListNode((summy + n1 + n2) % 10);
-        summy = parseInt((n1 + n2) / 10);
+        summy = parseInt((summy + n1 + n2) / 10);
         l3.next = temp;
         l3 = l3.next;
     }
